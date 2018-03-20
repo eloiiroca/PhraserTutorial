d20acd41a03db546d15dd67f17910f2b8906699c
Omar Garcia <omarcruz11@hotmai.com>
2018-03-20 17:37:40 +0100

    Refactoring

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -2,15 +2,13 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import { Switch } from 'react-router-dom';
 import './App.css';
-import Game from '../Game/Game';
 import Phrase from '../Phrase/Phrase'
 
 class App extends Component {
   render() {
     return(
       <Switch>
-        <Route exact path='/' component={Game}/>
-        <Route path='/roster' component={Phrase}/> 
+        <Route exact path='/' component={Phrase}/>
         {/*<Route path='/schedule' component={Schedule}/> */}
       </Switch>
     )
